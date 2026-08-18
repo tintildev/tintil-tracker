39e28c0b31669d58089df5bf71f89a00f5dd77ef
tintil / tintildev <34619286+tintildev@users.noreply.github.com>
2026-08-18 22:17:02 +0200

    Work in Progress, work on task class

--- src/main/java/com/tintil/tintiltracker/model/Task.java
+++ src/main/java/com/tintil/tintiltracker/model/Task.java
@@ -1,10 +1,25 @@
 package com.tintil.tintiltracker.model;
 
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+
+/**
+ * 
+ * Task Verwaltet die Aufgaben, mit den Eigenschaften @parm id, title, starteDate, dueDate
+ * 
+ * @author tintil
+ * @version 1.0
+ */
 
 @Entity
 @Table(name = "task")
@@ -13,6 +28,41 @@ public class Task {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    private String description;
+
+    private LocalDate startDate;
+    private LocalDate dueDate;
+
+    // @Enumerated(EnumType.STRING) speichert "TODO", "IN_PROGRESS" etc. als Text in der DB,
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private TaskStatus status;
+
+    @ManyToOne //Many tasks to one project
+    @JoinColumn(name= "project_id", nullable = false)
+    private Project project;
+
+    //constructor for JPA/Hibernate
+    public Task(){
+
+    }
+
+    //
+    public Task(String title, String description, LocalDate startDate, LocalDate dueDate, Project project){
+        this.title = title;
+        this.description = description;
+        this.startDate = startDate;
+        this.dueDate = dueDate;
+        this.status = status;
+        this.project = project;
+    }
+
+    // --- Getters und Setters
+    //TODO: Getters and Setters erstellen
 
 }
 
